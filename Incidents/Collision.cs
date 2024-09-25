5a02aa68ff53ade07b6a2666e1b7aa0214cfee50
DevKilo1 <luknik2012@gmail.com>
2024-09-25 16:22:46 -0400

    - Finished Collision.cs OnTrigger method.

--- Incidents/Collision.cs
+++ Incidents/Collision.cs
@@ -24,49 +24,49 @@ public class Collision() : BaseIncident<Collision>()
                 API.HasEntityBeenDamagedByEntity(Game.PlayerPed.CurrentVehicle.Handle, v.Handle, true)).ToList();
         List<Ped> pedsInvolved = new List<Ped>();
         
+        Utils.ShowNotification("~r~Pull over~s~ with the vehicle and ~y~wait~s~ for the ~f~supervisor~s~ to arrive on-scene.");
+        
         foreach (var v in vehiclesInvolved)
         {
+            API.ClearEntityLastDamageEntity(Game.PlayerPed.CurrentVehicle.Handle);
             Utils.CaptureEntity(v);
-            new Action(async () =>
+            var veh = await SpawnServiceVehicle((VehicleHash)v.Model.Hash);
+            foreach (var p in v.Occupants)
             {
-                var veh = await SpawnServiceVehicle((VehicleHash)v.Model.Hash);
-                foreach (var p in v.Occupants)
-                {
-                    Utils.CaptureEntity(p);
-                    var clone = p.Clone();
-                    var seat = (VehicleSeat)p.SeatIndex;
-                    clone.SetIntoVehicle(veh, seat);
-                    clone.Health = p.Health;
-                    pedsInvolved.Add(clone);
-                    p.Delete();
-                }
+                Utils.CaptureEntity(p);
+                var clone = p.Clone();
+                var seat = (VehicleSeat)p.SeatIndex;
+                clone.SetIntoVehicle(veh, seat);
+                clone.Health = p.Health;
+                pedsInvolved.Add(clone);
+                p.Delete();
+            }
 
-                API.CopyVehicleDamages(v.Handle, veh.Handle);
-                veh.Mods.Livery = v.Mods.Livery;
-                veh.Mods.ColorCombination = v.Mods.ColorCombination;
-                Vector3 pos = v.Position;
-                float heading = v.Heading;
-                v.Delete();
-                veh.Position = pos;
-                veh.Heading = heading;
-                var m = new Utils.Marker(MarkerType.ThickChevronUp, Utils.MarkerAttachTo.Entity, veh.Position, veh);
-                m.SetOffset(new(0f, 0f, 2f));
-                m.SetMovement(true, true);
-                m.SetColor(255,0,0);
-                m.SetRotation(new(0f, 180f,0f));
-                veh.IsEngineRunning = true;
-                var parkPos = await Utils.HandlePark(veh);
-                IncidentPosition = (Vector3)parkPos;
-            })();
+            API.CopyVehicleDamages(v.Handle, veh.Handle);
+            veh.Mods.Livery = v.Mods.Livery;
+            veh.Mods.ColorCombination = v.Mods.ColorCombination;
+            Vector3 pos = v.Position;
+            float heading = v.Heading;
+            v.Delete();
+            veh.Position = pos;
+            veh.Heading = heading;
+            var m = new Utils.Marker(MarkerType.ThickChevronUp, Utils.MarkerAttachTo.Entity, veh.Position, veh);
+            m.SetOffset(new(0f, 0f, 2f));
+            m.SetMovement(true, true);
+            m.SetColor(255,0,0);
+            m.SetRotation(new(0f, 180f,0f));
+            veh.IsEngineRunning = true;
+            var parkPos = await Utils.HandlePark(veh);
+            IncidentPosition = (Vector3)parkPos;
         }
+        
         // Spawn AI
-
         var supervisorVeh = await this.SpawnServiceVehicle(VehicleHash.Police);
         supervisorVeh.IsEngineRunning = true;
         var supervisor = await this.SpawnServicePed(PedHash.Cop01SMY, supervisorVeh, VehicleSeat.Driver);
         supervisor.AttachBlip();
         var supe = new Utils.Waypoint(IncidentPosition, supervisorVeh);
-        supe.SetDrivingSpeed(10f);
+        supe.SetDrivingSpeed(5f);
         //supervisor.Task.DriveTo(supervisorVeh, IncidentPosition, supervisorVeh.Model.GetDimensions().Length() / 2, 20f, supe.DrivingStyle);
         Utils.SlowVehicleDownInRadiusToPosition(supervisorVeh, IncidentPosition.ClosestParkedCarPlacement(), 10f, supe.DrivingStyle);
         supe.Start();
